13b13ee3011edac3366bf456aff7e5cb1757c660
Anjach1 <chamodhyaanjana@gmail.com>
2026-03-20 10:59:40 +0530

    Final commit

--- src/config/dbConnection.js
+++ src/config/dbConnection.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
     try{
-        const connect = await mongoose.connect(process.env.CONNECTION_STRING);
+        const connect = await mongoose.connect(process.env.MONGO_URI);
         console.log("Database connected successfully...",
         connect.connection.host,
         connect.connection.name);
